ddabe684f03b2b6c6e87775df060aaa87582e9b6
huangjianhehehe <786762595@qq.com>
2020-10-11 18:52:04 +0800

    Alcor ,Camera  增加计数器

--- cyusbDemo/Form1.Designer.cs
+++ cyusbDemo/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace cyusbDemo
 {
-    partial class Form1
+    partial class WinUpan
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -29,12 +29,267 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinUpan));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_Number = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "alcor  and camera";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(106, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "delay (ms):";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "650";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "200",
+            "400",
+            "650",
+            "800"});
+            this.comboBox1.Location = new System.Drawing.Point(71, 76);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(58, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(149, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 74);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "SET";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(149, 150);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(56, 23);
+            this.btn.TabIndex = 9;
+            this.btn.Text = "WriteTo";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Visible = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(0, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "TestTotal:";
+            // 
+            // lbl_Number
+            // 
+            this.lbl_Number.AutoSize = true;
+            this.lbl_Number.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_Number.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbl_Number.Location = new System.Drawing.Point(115, 5);
+            this.lbl_Number.Name = "lbl_Number";
+            this.lbl_Number.Size = new System.Drawing.Size(159, 64);
+            this.lbl_Number.TabIndex = 1;
+            this.lbl_Number.Text = "Start ";
+            this.lbl_Number.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Number.Click += new System.EventHandler(this.lbl_Number_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "cNumber:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DisplayMember = "0";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "100",
+            "200",
+            "300",
+            "400",
+            "500",
+            "600",
+            "700",
+            "800",
+            "900",
+            "1000",
+            "",
+            "",
+            ""});
+            this.comboBox2.Location = new System.Drawing.Point(71, 98);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(49, 20);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.ValueMember = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "num_Alcor:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "1",
+            "5",
+            "8",
+            "15",
+            "20"});
+            this.comboBox3.Location = new System.Drawing.Point(71, 124);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(49, 20);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "loopTime:";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.comboBox4.Location = new System.Drawing.Point(71, 150);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(49, 20);
+            this.comboBox4.TabIndex = 8;
+            // 
+            // WinUpan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(282, 176);
+            this.Controls.Add(this.lbl_Number);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WinUpan";
+            this.Opacity = 0.8D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "USB8";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Yellow;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WinUpan_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.WinUpan_MouseDown);
+            this.MouseLeave += new System.EventHandler(this.WinUpan_MouseLeave);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_Number;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox4;
     }
 }
 
--- cyusbDemo/Form1.cs
+++ cyusbDemo/Form1.cs
@@ -1,19 +1,351 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace cyusbDemo
 {
-    public partial class Form1 : Form
+    public partial class WinUpan : Form
     {
-        public Form1()
+        private string IniFilePath;
+        int count = 0;     //u盘个数(中间量)
+        int cNumber = 0;    //测试计数
+        int num_Alcor = 8;   // 待测试安国本体数量
+        int delay = 150;      //延时时间
+        int loopTime = 1;   // 空格次数
+        string Section = "Information";
+        int cN = 0;
+       
+
+        #region U盘设备相关常量
+        public const int WM_DEVICECHANGE = 0x219;//U盘插入后，OS的底层会自动检测到，然后向应用程序发送“硬件设备状态改变“的消息
+        public const int DBT_DEVICEARRIVAL = 0x8000;  //就是用来表示U盘可用的。一个设备或媒体已被插入一块，现在可用。
+        public const int DBT_CONFIGCHANGECANCELED = 0x0019;  //要求更改当前的配置（或取消停靠码头）已被取消。
+        public const int DBT_CONFIGCHANGED = 0x0018;  //当前的配置发生了变化，由于码头或取消固定。
+        public const int DBT_CUSTOMEVENT = 0x8006; //自定义的事件发生。 的Windows NT 4.0和Windows 95：此值不支持。
+        public const int DBT_DEVICEQUERYREMOVE = 0x8001;  //审批要求删除一个设备或媒体作品。任何应用程序也不能否认这一要求，并取消删除。
+        public const int DBT_DEVICEQUERYREMOVEFAILED = 0x8002;  //请求删除一个设备或媒体片已被取消。
+        public const int DBT_DEVICEREMOVECOMPLETE = 0x8004;  //一个设备或媒体片已被删除。
+        public const int DBT_DEVICEREMOVEPENDING = 0x8003;  //一个设备或媒体一块即将被删除。不能否认的。
+        public const int DBT_DEVICETYPESPECIFIC = 0x8005;  //一个设备特定事件发生。
+        public const int DBT_DEVNODES_CHANGED = 0x0007;  //一种设备已被添加到或从系统中删除。
+        public const int DBT_QUERYCHANGECONFIG = 0x0017;  //许可是要求改变目前的配置（码头或取消固定）。
+        public const int DBT_USERDEFINED = 0xFFFF;  //此消息的含义是用户定义的
+        public const uint GENERIC_READ = 0x80000000;
+        public const int GENERIC_WRITE = 0x40000000;
+        public const int FILE_SHARE_READ = 0x1;
+        public const int FILE_SHARE_WRITE = 0x2;
+        public const int IOCTL_STORAGE_EJECT_MEDIA = 0x2d4808;
+        #endregion
+
+        public WinUpan()
         {
             InitializeComponent();
+            count = 0;
+            this.Height = 70;
+            //Combobox.SelectedIndex = Combobox.Items.IndexOf(“默认选中文本”);
+            comboBox1.SelectedIndex = comboBox1.Items.IndexOf("200");
+            comboBox2.SelectedIndex = comboBox2.Items.IndexOf("0");
+            comboBox3.SelectedIndex = comboBox3.Items.IndexOf("8");
+            comboBox4.SelectedIndex = comboBox4.Items.IndexOf("1");
+            //=====判断进程法：(修改程序名字后依然能执行)=====
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            foreach (Process process in processes)
+            {
+                if (process.Id != current.Id)
+                {
+                    if (process.MainModule.FileName
+                    == current.MainModule.FileName)
+                    {
+                        MessageBox.Show("程序已经运行！", Application.ProductName,
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        Process.GetCurrentProcess().Kill();
+                        //return;
+                    }
+                }
+            }
+        }
+  
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            count = 0;
+            cN = 0;
+
+
+            button1_Click(sender, e);
+           
+
+        }
+
+        //获取配置文件中的值 
+        private void GetValue(string section, string key, out string value)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            GetPrivateProfileString(section, key, "", stringBuilder, 1024, IniFilePath);
+            value = stringBuilder.ToString();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            
+            try
+            {
+               
+                if (m.Msg==WM_DEVICECHANGE)
+                {
+                    
+                    cN++;
+                  
+                  
+                    switch (m.WParam.ToInt32())
+                    {
+                        case DBT_DEVNODES_CHANGED:
+                           
+                      
+
+                            break;
+
+                        case DBT_DEVICEARRIVAL:
+                           
+                            DriveInfo[] s = DriveInfo.GetDrives();
+                            s.Any(t =>
+                            {
+                                if (t.DriveType==DriveType.Removable)
+                                {
+                                    count++;
+                                    return true;
+                                }
+                               
+                                return false;
+                            });
+                            if (count==num_Alcor)
+                            {
+                                Thread.Sleep(delay);
+                                SendKeys.Send(" ");
+                               
+                            }
+                            else if((count<0)&&(count>8))
+                            {
+                                count = 0;
+                            }
+                           
+                            break;
+                        case DBT_DEVICEREMOVECOMPLETE:
+                           
+                            count--;
+                           
+                           
+                            if (count<0)
+                            {
+                                count = 0; 
+                            }
+                            break;
+                        default:
+                           
+                            break;
+                    }
+
+                    for (int i = 0; i < loopTime; i++)
+                    {
+                        //if (cN % 6 == 0 || cN % 7 == 0 || cN % 8 == 0)
+                        //{
+                            Thread.Sleep(delay);
+                            SendKeys.Send(" ");
+                        Thread.Sleep(delay);
+                        //}
+                    }
+                    // cN % 20 == 0 || cN % 12 == 0 cN % 18 == 0 |||| cN % 14 == 0 || cN % 16 == 0
+                    if ( cN % 6 == 0   )
+                    {
+                        
+                        cNumber++;
+                        lbl_Number.Text = cNumber.ToString();
+                        cN = 0;
+                        Thread.Sleep(delay);
+
+                    }
+
+                    WritePrivateProfileString(Section, "cNumber", lbl_Number.Text, IniFilePath);
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return;
+            }
+
+            
+            base.WndProc(ref m);
+
+          
+
+        }
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr CreateFile(
+         string lpFileName,
+         uint dwDesireAccess,
+         uint dwShareMode,
+         IntPtr SecurityAttributes,
+         uint dwCreationDisposition,
+         uint dwFlagsAndAttributes,
+         IntPtr hTemplateFile);
+
+        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool DeviceIoControl(
+            IntPtr hDevice,
+            uint dwIoControlCode,
+            IntPtr lpInBuffer,
+            uint nInBufferSize,
+            IntPtr lpOutBuffer,
+            uint nOutBufferSize,
+            out uint lpBytesReturned,
+            IntPtr lpOverlapped
+        );
+        [DllImport("kernel32.dll")]
+
+        private static extern long WritePrivateProfileString(string section, string key, string value, string filepath);
+
+
+
+        [DllImport("kernel32.dll")]
+
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder returnvalue, int buffersize, string filepath);
+
+
+        #region 无连框窗体拖动
+        //using System.Runtime.InteropServices;
+ 
+    [DllImport("user32.dll")]
+    public static extern bool ReleaseCapture();
+    [DllImport("user32.dll")]
+    public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+    public const int WM_SYSCOMMAND = 0x0112;
+    public const int SC_MOVE = 0xF010;
+    public const int HTCAPTION = 0x0002;
+
+
+    #endregion
+
+
+
+
+    private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void WinUpan_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();  //点X只隐藏主窗体
+            e.Cancel = true;
+
+        }
+        #region 单击按钮写入到配置文件中,这个有点多余,本程序不使用
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btn_Click(sender, e);
+            string outString;
+            IniFilePath = Application.StartupPath + "\\Config.ini";
+            try
+            {
+
+                GetValue("Information", "Delay", out outString);
+                delay = Convert.ToInt32(outString);
+                GetValue("Information", "cNumber", out outString);
+                cNumber = Convert.ToInt32(outString);
+                lbl_Number.Text = cNumber.ToString();
+                GetValue("Information", "num_Alcor", out outString);
+                num_Alcor = Convert.ToInt32(outString);
+                GetValue("Information", "loopTime", out outString);
+                loopTime = Convert.ToInt32(outString);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Trim() != "")
+            {
+                string Section = "Information";
+                try
+                {
+
+                    WritePrivateProfileString(Section, "Delay", comboBox1.Text, IniFilePath);
+                    WritePrivateProfileString(Section, "cNumber", comboBox2.Text, IniFilePath);
+                    WritePrivateProfileString(Section, "num_Alcor", comboBox3.Text, IniFilePath);
+                    WritePrivateProfileString(Section, "loopTime", comboBox4.Text, IniFilePath);
+
+
+                }
+                catch (Exception ee)
+                {
+
+                    MessageBox.Show(ee.Message);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("不能为空", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+
+
+
+        private void WinUpan_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);
+
+        }
+
+        private void lbl_Number_Click(object sender, EventArgs e)
+        {
+            this.Height = 180;
+            //Thread.Sleep(30000);
+            
+        }
+
+        private void WinUpan_MouseLeave(object sender, EventArgs e)
+        {
+            //this.Height = 70;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Height = 70;
+
         }
     }
+    
 }
+
